ff2a87edc82dcc72ce8b2b1e0f95741807ea917d
Brian Silah <unpervertedkid@gmail.com>
2023-05-17 12:03:51 +0300

    02-shallow-differences solution

--- projects/arrays/analyzing-dna/02-shallow-differences/index.ts
+++ projects/arrays/analyzing-dna/02-shallow-differences/index.ts
@@ -1,2 +1,16 @@
 // Write your shallowDifferences function here! ✨
+function shallowDifferences(
+	a: string[],
+	b: string[]
+): (string | undefined)[] | undefined {
+	if (a.length !== b.length) {
+		return undefined;
+	}
+	let differences: (string | undefined)[] = [];
+	for (let i = 0; i < a.length; i++) {
+		differences.push(a[i] === b[i] ? a[i] : undefined);
+	}
+	return differences;
+}
 // You'll need to export it so the tests can run it.
+export { shallowDifferences };
