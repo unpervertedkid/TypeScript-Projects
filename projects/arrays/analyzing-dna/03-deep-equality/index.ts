93b13be3b730324c75387eac8440ff647255ac65
Brian Silah <unpervertedkid@gmail.com>
2023-05-17 15:15:23 +0300

    03-deep-equality solution

--- projects/arrays/analyzing-dna/03-deep-equality/index.ts
+++ projects/arrays/analyzing-dna/03-deep-equality/index.ts
@@ -1,2 +1,13 @@
 // Write your deepEquality function here! ✨
+function deepEquality(a: string[][], b: string[][]): boolean {
+	if (a.length !== b.length) return false;
+	for (let i = 0; i < a.length; i++) {
+		if (a[i].length !== b[i].length) return false;
+		for (let j = 0; j < a[i].length; j++) {
+			if (a[i][j] !== b[i][j]) return false;
+		}
+	}
+	return true;
+}
 // You'll need to export it so the tests can run it.
+export { deepEquality };
