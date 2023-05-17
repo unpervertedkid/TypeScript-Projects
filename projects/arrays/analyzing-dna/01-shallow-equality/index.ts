4aad44fea53923c24bc15a7584068dc7796400cc
Brian Silah <unpervertedkid@gmail.com>
2023-05-17 11:52:48 +0300

    01-shallow-equality solution

--- projects/arrays/analyzing-dna/01-shallow-equality/index.ts
+++ projects/arrays/analyzing-dna/01-shallow-equality/index.ts
@@ -1,2 +1,10 @@
 // Write your shallowEquality function here! ✨
+function shallowEquality(a: string[], b: string[]): boolean {
+	if (a.length !== b.length) return false;
+	for (let i = 0; i < a.length; i += 1) {
+		if (a[i] !== b[i]) return false;
+	}
+	return true;
+}
 // You'll need to export it so the tests can run it.
+export { shallowEquality };
