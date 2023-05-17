a2b109dada1e5765a53cafedbbde3d66c7a3756f
Brian Silah <unpervertedkid@gmail.com>
2023-05-17 15:40:45 +0300

    From JavaScript to typescript 01-syntactic-sugar

--- projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
@@ -1,4 +1,21 @@
 // Put your announceMachines function here! ✨
+function announceMachines(announce, ...machines) {
+	let labelsCount = 0;
+
+	for (const machine of machines) {
+		let label;
+
+		if (machine.label) {
+			label = machine.label;
+			labelsCount += 1;
+		} else {
+			label = `Make: ${machine.make}; Model: ${machine.model}`;
+		}
+
+		announce(label);
+	}
+	return labelsCount;
+}
 // See ./original.js for its older JavaScript code.
 
 module.exports.announceMachines = announceMachines;
